1539379ad538a3517466dbbc8ea206a902787ac8
benasbart <bbart23@student.sdu.dk>
2024-05-20 21:49:00 +0200

    optimization tab

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,12 +5,14 @@
         public BoilersViewModel BoilersViewModel { get; }
         public HeatingViewModel HeatingViewModel { get; }
         public GraphViewModel GraphViewModel{ get; }
+        public OptimizerViewModel OptimizerViewModel{ get; }
 
         public MainWindowViewModel()
         {
             BoilersViewModel = new BoilersViewModel();
             HeatingViewModel = new HeatingViewModel();
             GraphViewModel = new GraphViewModel();
+            OptimizerViewModel = new OptimizerViewModel();
         }
     }
 }
