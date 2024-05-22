462f70b8dac20894293209b4a6af244f50344741
s1masy <simonas.tarasauskas@gmail.com>
2024-05-23 00:25:34 +0200

    More information in graphs and proper titles

--- ViewModels/GraphViewModel.cs
+++ ViewModels/GraphViewModel.cs
@@ -102,34 +102,63 @@ namespace OptiHeatPro.ViewModels
             new StackedStepAreaSeries<double>
             {
                 Name = "Gas Boiler",
-                //Fill = new SolidColorPaint(SKColors.Red),
-                Values = SGasBoilerOutput
+                Fill = new SolidColorPaint(SKColors.Red.WithAlpha(100)),
+                Values = SGasBoilerOutput,
+                ScalesYAt = 0
             },
             new StackedStepAreaSeries<double>
             {
                 Name = "Oil Boiler",
-                //Fill = new SolidColorPaint(SKColors.Green),
-                Values = SOilBoilerOutput
+                Fill = new SolidColorPaint(SKColors.SlateGray.WithAlpha(100)),
+                Values = SOilBoilerOutput,
+                ScalesYAt = 0
             },
             new StackedStepAreaSeries<double>
             {
                 Name = "Gas Motor",
-                //Fill = new SolidColorPaint(SKColors.Yellow),
-                Values = SGasMotorOutput
+                Fill = new SolidColorPaint(SKColors.YellowGreen.WithAlpha(150)),
+                Values = SGasMotorOutput,
+                ScalesYAt = 0
             },
             new StackedStepAreaSeries<double>
             {
                 Name = "Electric Boiler",
-                //Fill = new SolidColorPaint(SKColors.Yellow),
-                Values = SElectricBoilerOutput
+                Fill = new SolidColorPaint(SKColors.CadetBlue.WithAlpha(200)),
+                Values = SElectricBoilerOutput,
+                ScalesYAt = 0
+            },
+            new StepLineSeries<double>
+            {
+                Name = "Heat Demand",
+                Values = SHeatDemand,
+                GeometrySize = 0,
+                Stroke = new SolidColorPaint(SKColors.DarkSlateGray) {StrokeThickness = 5},
+                GeometryFill = new SolidColorPaint(SKColors.DarkSlateGray),
+                GeometryStroke = new SolidColorPaint(SKColors.DarkSlateGray),
+                Fill = null,
+                ScalesYAt = 0
+            },
+            new LineSeries<decimal>
+            {
+                Name= "Electricity Price",
+                Stroke = new SolidColorPaint(SKColors.Black) {StrokeThickness = 3},
+                GeometryFill = new SolidColorPaint(SKColors.Black),
+                GeometryStroke = new SolidColorPaint(SKColors.Black),
+                Fill = null,
+                Values = SElectricityPrice,
+                GeometrySize = 0,
+                LineSmoothness = 1,
+                ScalesYAt = 1
             }
         };
         public ISeries[] SummerElectricityPrices { get; set; } =
         {
             new StepLineSeries<decimal>
             {
+                Name = "Electricity Price",
                 Values = SElectricityPrice,
-                Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -139,8 +168,10 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<double>
             {
+                Name = "Produced Electricity",
                 Values = STotalElectricityProduction,
-                Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -151,13 +182,13 @@ namespace OptiHeatPro.ViewModels
             new StackedStepAreaSeries<double>
             {
                 Name = "Gas",
-                //Fill = new SolidColorPaint(SKColors.Red),
+                Fill = new SolidColorPaint(SKColors.YellowGreen.WithAlpha(150)),
                 Values = STotalGasConsumption
             },
             new StackedStepAreaSeries<double>
             {
                 Name = "Oil",
-                //Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.SlateGray.WithAlpha(100)),
                 Values = STotalOilConsumption
             }
         };
@@ -165,8 +196,10 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<double>
             {
+                Name = "Heat Demand",
                 Values = SHeatDemand,
-                Fill = new SolidColorPaint(SKColors.DarkRed),
+                Fill = new SolidColorPaint(SKColors.Red.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Red.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -176,8 +209,10 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<decimal>
             {
+                Name = "Production Cost",
                 Values = STotalProductionCost,
-                Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -187,47 +222,77 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<double>
             {
+                Name = "CO2 Emissions",
                 Values = STotalCO2Emissions,
-                Fill = new SolidColorPaint(SKColors.SlateGray),
+                Fill = new SolidColorPaint(SKColors.SlateGray.WithAlpha(200)),
+                Stroke = new SolidColorPaint(SKColors.SlateGray.WithAlpha(200)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
             }
         };
-
-                public ISeries[] Winter { get; set; } =
+        public ISeries[] Winter { get; set; } =
         {
             new StackedStepAreaSeries<double>
             {
-                Name = "Gas Motor",
-                //Fill = new SolidColorPaint(SKColors.Yellow),
-                Values = WGasMotorOutput
-            },
-            new StackedStepAreaSeries<double>
-            {
                 Name = "Gas Boiler",
-                //Fill = new SolidColorPaint(SKColors.Red),
-                Values = WGasBoilerOutput
+                Fill = new SolidColorPaint(SKColors.Red.WithAlpha(100)),
+                Values = WGasBoilerOutput,
+                ScalesYAt = 0
             },
             new StackedStepAreaSeries<double>
             {
                 Name = "Oil Boiler",
-                //Fill = new SolidColorPaint(SKColors.Green),
-                Values = WOilBoilerOutput
+                Fill = new SolidColorPaint(SKColors.SlateGray.WithAlpha(100)),
+                Values = WOilBoilerOutput,
+                ScalesYAt = 0
+            },
+            new StackedStepAreaSeries<double>
+            {
+                Name = "Gas Motor",
+                Fill = new SolidColorPaint(SKColors.YellowGreen.WithAlpha(150)),
+                Values = WGasMotorOutput,
+                ScalesYAt = 0
             },
             new StackedStepAreaSeries<double>
             {
                 Name = "Electric Boiler",
-                //Fill = new SolidColorPaint(SKColors.Yellow),
-                Values = WElectricBoilerOutput
+                Fill = new SolidColorPaint(SKColors.CadetBlue.WithAlpha(200)),
+                Values = WElectricBoilerOutput,
+                ScalesYAt = 0
+            },
+            new StepLineSeries<double>
+            {
+                Name = "Heat Demand",
+                Values = WHeatDemand,
+                GeometrySize = 0,
+                Stroke = new SolidColorPaint(SKColors.DarkSlateGray) {StrokeThickness = 5},
+                GeometryFill = new SolidColorPaint(SKColors.DarkSlateGray),
+                GeometryStroke = new SolidColorPaint(SKColors.DarkSlateGray),
+                Fill = null,
+                ScalesYAt = 0
+            },
+            new LineSeries<decimal>
+            {
+                Name= "Electricity Price",
+                Stroke = new SolidColorPaint(SKColors.Black) {StrokeThickness = 3},
+                GeometryFill = new SolidColorPaint(SKColors.Black),
+                GeometryStroke = new SolidColorPaint(SKColors.Black),
+                Fill = null,
+                Values = WElectricityPrice,
+                GeometrySize = 0,
+                LineSmoothness = 1,
+                ScalesYAt = 1
             }
         };
         public ISeries[] WinterElectricityPrices { get; set; } =
         {
             new StepLineSeries<decimal>
             {
+                Name = "Electricity Price",
                 Values = WElectricityPrice,
-                Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -237,8 +302,10 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<double>
             {
+                Name = "Produced Electricity",
                 Values = WTotalElectricityProduction,
-                Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -249,13 +316,13 @@ namespace OptiHeatPro.ViewModels
             new StackedStepAreaSeries<double>
             {
                 Name = "Gas",
-                //Fill = new SolidColorPaint(SKColors.Red),
+                Fill = new SolidColorPaint(SKColors.YellowGreen.WithAlpha(150)),
                 Values = WTotalGasConsumption
             },
             new StackedStepAreaSeries<double>
             {
                 Name = "Oil",
-                //Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.SlateGray.WithAlpha(100)),
                 Values = WTotalOilConsumption
             }
         };
@@ -263,8 +330,10 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<double>
             {
+                Name = "Heat Demand",
                 Values = WHeatDemand,
-                Fill = new SolidColorPaint(SKColors.DarkRed),
+                Fill = new SolidColorPaint(SKColors.Red.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Red.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -274,8 +343,10 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<decimal>
             {
+                Name = "Production Cost",
                 Values = WTotalProductionCost,
-                Fill = new SolidColorPaint(SKColors.Green),
+                Fill = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
+                Stroke = new SolidColorPaint(SKColors.Green.WithAlpha(150)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -285,8 +356,10 @@ namespace OptiHeatPro.ViewModels
         {
             new StepLineSeries<double>
             {
+                Name = "CO2 Emissions",
                 Values = WTotalCO2Emissions,
-                Fill = new SolidColorPaint(SKColors.SlateGray),
+                Fill = new SolidColorPaint(SKColors.SlateGray.WithAlpha(200)),
+                Stroke = new SolidColorPaint(SKColors.SlateGray.WithAlpha(200)),
                 GeometrySize = 0,
                 GeometryFill = null,
                 GeometryStroke = null
@@ -298,7 +371,7 @@ namespace OptiHeatPro.ViewModels
             {
                 new Axis
                 {
-                    Labels = WDnT
+                    Labels = WDnT,
                 }
             };
         public Axis[] SXAxes { get; set; }
@@ -306,7 +379,25 @@ namespace OptiHeatPro.ViewModels
             {
                 new Axis
                 {
-                    Labels = SDnT
+                    Labels = SDnT,
+                }
+            };
+        public Axis[] HEYAxes { get; set; }
+            = new Axis[]
+            {
+                
+                new Axis
+                {
+                    //Name = "MW",
+                    Labeler = (value) => Math.Round(value,2) + " MW",
+                    MinLimit = 0
+                },
+                new Axis
+                {
+                    //Name = "DKK",
+                    Labeler = (value) => Math.Round(value,2) + " DKK",
+                    Position = LiveChartsCore.Measure.AxisPosition.End,
+                    MinLimit = 0
                 }
             };
         public Axis[] ElYAxes { get; set; }
@@ -324,7 +415,8 @@ namespace OptiHeatPro.ViewModels
                 new Axis
                 {
                     //Name = "MW",
-                    Labeler = (value) => Math.Round(value,2) + " MW"
+                    Labeler = (value) => Math.Round(value,2) + " MW",
+                    MinLimit = 0
                 }
             };
         public Axis[] CYAxes { get; set; }
@@ -342,12 +434,69 @@ namespace OptiHeatPro.ViewModels
                 new Axis
                 {
                     //Name = "kg",
-                    Labeler = (value) => Math.Round(value,2) + " kg"
+                    Labeler = (value) => Math.Round(value,2) + " kg",
+                    MinLimit = 0
                 }
             };
         public DrawMarginFrame DrawMarginFrame => new DrawMarginFrame
         {
             Stroke = new SolidColorPaint(SKColors.DimGray, 1)
+        };
+        public LabelVisual HPTitle { get; set; } =
+        new LabelVisual
+        {
+            Text = "Heat Production",
+            TextSize = 20,
+            Padding = new LiveChartsCore.Drawing.Padding(5),
+            Paint = new SolidColorPaint(SKColors.Black)
+        };
+        public LabelVisual HDTitle { get; set; } =
+        new LabelVisual
+        {
+            Text = "Heat Demand",
+            TextSize = 20,
+            Padding = new LiveChartsCore.Drawing.Padding(5),
+            Paint = new SolidColorPaint(SKColors.Black)
+        };
+        public LabelVisual PCTitle { get; set; } =
+        new LabelVisual
+        {
+            Text = "Production Costs",
+            TextSize = 20,
+            Padding = new LiveChartsCore.Drawing.Padding(5),
+            Paint = new SolidColorPaint(SKColors.Black)
+        };
+        public LabelVisual EPTitle { get; set; } =
+        new LabelVisual
+        {
+            Text = "Electricity Prices",
+            TextSize = 20,
+            Padding = new LiveChartsCore.Drawing.Padding(5),
+            Paint = new SolidColorPaint(SKColors.Black)
+        };
+        public LabelVisual EPDTitle { get; set; } =
+        new LabelVisual
+        {
+            Text = "Electricity Production",
+            TextSize = 20,
+            Padding = new LiveChartsCore.Drawing.Padding(5),
+            Paint = new SolidColorPaint(SKColors.Black)
+        };
+        public LabelVisual FCTitle { get; set; } =
+        new LabelVisual
+        {
+            Text = "Fuel Consumption",
+            TextSize = 20,
+            Padding = new LiveChartsCore.Drawing.Padding(5),
+            Paint = new SolidColorPaint(SKColors.Black)
+        };
+        public LabelVisual ETitle { get; set; } =
+        new LabelVisual
+        {
+            Text = "CO2 Emissions",
+            TextSize = 20,
+            Padding = new LiveChartsCore.Drawing.Padding(5),
+            Paint = new SolidColorPaint(SKColors.Black)
         };
     }
 }
