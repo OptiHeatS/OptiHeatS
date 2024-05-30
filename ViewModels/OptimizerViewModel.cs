444d6b1a60879dac15fe180917ba5a93e3109ab2
benasbart <bbart23@student.sdu.dk>
2024-05-30 20:31:47 +0200

    fixed viewmodel for slider

--- ViewModels/OptimizerViewModel.cs
+++ ViewModels/OptimizerViewModel.cs
@@ -5,49 +5,74 @@ namespace OptiHeatPro.ViewModels
 {
     public class OptimizerViewModel : ViewModelBase
     {
-        private Result _WtotalResult;
+        private double _co2ReductionPercentage;
+        public double co2ReductionPercentage
+        {
+            get => _co2ReductionPercentage;
+            set => this.RaiseAndSetIfChanged(ref _co2ReductionPercentage, value);
+        }
+
+        private Result _wTotalResult;
         public Result WTotalResult
         {
-            get => _WtotalResult;
-            set => this.RaiseAndSetIfChanged(ref _WtotalResult, value);
+            get => _wTotalResult;
+            set => this.RaiseAndSetIfChanged(ref _wTotalResult, value);
         }
-        private Result _StotalResult;
+
+        private Result _sTotalResult;
         public Result STotalResult
         {
-            get => _StotalResult;
-            set => this.RaiseAndSetIfChanged(ref _StotalResult, value);
+            get => _sTotalResult;
+            set => this.RaiseAndSetIfChanged(ref _sTotalResult, value);
         }
 
-        
         public OptimizerViewModel()
         {
-            HeatingData _heatingData = new HeatingData();
-            _heatingData.Read();
-            Optimizer optimizer = new Optimizer();
-            List<Result> results = optimizer.Optimize(_heatingData.WinterData, 0);
-
             WTotalResult = new Result();
-            foreach (var result in results)
-            {
-            WTotalResult.TotalElectricityProduction += result.TotalElectricityProduction;
-            WTotalResult.TotalProductionCost += result.TotalProductionCost;
-            WTotalResult.TotalGasConsumption += result.TotalGasConsumption;
-            WTotalResult.TotalOilConsumption += result.TotalOilConsumption;
-            WTotalResult.TotalElectricityConsumption += result.TotalElectricityConsumption;
-            WTotalResult.TotalCO2Emissions += result.TotalCO2Emissions;
-            }
-            results = optimizer.Optimize(_heatingData.SummerData, 0);
-
             STotalResult = new Result();
+            this.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(co2ReductionPercentage))
+                {
+                    CalculateResults();
+                }
+            };
+
+            CalculateResults();
+        }
+
+        private void CalculateResults()
+        {
+            HeatingData heatingData = new HeatingData();
+            heatingData.Read();
+            Optimizer optimizer = new Optimizer();
+            List<Result> results = optimizer.Optimize(heatingData.WinterData, co2ReductionPercentage);
+
+            Result newWTotalResult = new Result();
             foreach (var result in results)
             {
-            STotalResult.TotalElectricityProduction += result.TotalElectricityProduction;
-            STotalResult.TotalProductionCost += result.TotalProductionCost;
-            STotalResult.TotalGasConsumption += result.TotalGasConsumption;
-            STotalResult.TotalOilConsumption += result.TotalOilConsumption;
-            STotalResult.TotalElectricityConsumption += result.TotalElectricityConsumption;
-            STotalResult.TotalCO2Emissions += result.TotalCO2Emissions;
+                newWTotalResult.TotalElectricityProduction += result.TotalElectricityProduction;
+                newWTotalResult.TotalProductionCost += result.TotalProductionCost;
+                newWTotalResult.TotalGasConsumption += result.TotalGasConsumption;
+                newWTotalResult.TotalOilConsumption += result.TotalOilConsumption;
+                newWTotalResult.TotalElectricityConsumption += result.TotalElectricityConsumption;
+                newWTotalResult.TotalCO2Emissions += result.TotalCO2Emissions;
             }
+            WTotalResult = newWTotalResult;
+
+            results = optimizer.Optimize(heatingData.SummerData, co2ReductionPercentage);
+
+            Result newSTotalResult = new Result();
+            foreach (var result in results)
+            {
+                newSTotalResult.TotalElectricityProduction += result.TotalElectricityProduction;
+                newSTotalResult.TotalProductionCost += result.TotalProductionCost;
+                newSTotalResult.TotalGasConsumption += result.TotalGasConsumption;
+                newSTotalResult.TotalOilConsumption += result.TotalOilConsumption;
+                newSTotalResult.TotalElectricityConsumption += result.TotalElectricityConsumption;
+                newSTotalResult.TotalCO2Emissions += result.TotalCO2Emissions;
+            }
+            STotalResult = newSTotalResult;
         }
     }
 }
